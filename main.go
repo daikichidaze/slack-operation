0135cfaad60d55fa2d50cf2f4862b423f369de4f
daikichidaze <dkch.yoshioka.t@gmail.com>
2021-12-30 15:14:21 +0700

    add sleep to api recovery time

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/slack-go/slack"
 )
@@ -20,33 +21,37 @@ func main() {
 	flag.Parse()
 	tkn := flag.Arg(0)
 	api := slack.New(tkn)
-	_, chn_id := get_calendar_channel(api)
+	chn_id := get_calendar_channel(api)
 
 	prm := slack.GetConversationHistoryParameters{ChannelID: chn_id}
 	res, err := api.GetConversationHistory(&prm)
 	check(err)
 
 	delete_title := "た：みーてぃんぐ"
+	var cnt int
 
 	for _, m := range res.Messages {
 		for _, att := range m.Attachments {
 			if att.Title == delete_title {
-				a, b, err := api.DeleteMessage(chn_id, m.Timestamp)
+				_, _, err := api.DeleteMessage(chn_id, m.Timestamp)
 				check(err)
-				fmt.Println(a, b)
+				time.Sleep(1 * time.Second)
+				cnt++
 			}
 		}
 	}
+	fmt.Println(cnt, "messages were deleted")
 
 }
 
-func get_calendar_channel(api *slack.Client) (*slack.Channel, string) {
+func get_calendar_channel(api *slack.Client) string {
 	var result string
 	calender_name := "calendar"
 	prm := slack.GetConversationsParameters{}
 
 	channels, _, err := api.GetConversations(&prm)
 	check(err)
+	time.Sleep(1 * time.Second)
 
 	for _, c := range channels {
 
@@ -55,9 +60,6 @@ func get_calendar_channel(api *slack.Client) (*slack.Channel, string) {
 		}
 	}
 
-	c, err := api.GetConversationInfo(result, true)
-	check(err)
-
-	return c, result
+	return result
 
 }
