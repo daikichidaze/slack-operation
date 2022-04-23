5da5da87ed38d24f8277b93d9482c1e934aac889
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-04-23 10:22:25 +0700

    Update module arguments

--- main.go
+++ main.go
@@ -22,33 +22,36 @@ func main() {
 	tkn := flag.Arg(0)
 	api := slack.New(tkn)
 
-	chn_id := get_calendar_channel(api) // Get target channel id
+	channel_name := flag.Arg(1)                       // Target name of channel
+	chn_id := get_calendar_channel(api, channel_name) // Get target channel id
 
 	// Get message list
 	prm := slack.GetConversationHistoryParameters{ChannelID: chn_id, Limit: 1000}
 	res, err := api.GetConversationHistory(&prm)
 	check(err)
 
-	delete_title := "hogehoge" // Target title name to delete
+	delete_title := flag.Arg(2) // Target title name to delete
 	var cnt int
 
 	for _, m := range res.Messages {
+		// For google calendar
 		for _, att := range m.Attachments {
 			if att.Title == delete_title {
-				_, _, err := api.DeleteMessage(chn_id, m.Timestamp)
-				check(err)
-				time.Sleep(1 * time.Second) // To escape the api limit
-				cnt++
+				cnt = delete_message(api, chn_id, m.Timestamp, cnt)
 			}
 		}
+
+		//For tobuy
+		// if strings.Contains(m.Text, delete_title) {
+		// 	cnt = delete_message(api, chn_id, m.Timestamp, cnt)
+		// }
 	}
 	fmt.Println(cnt, "messages were deleted")
 
 }
 
-func get_calendar_channel(api *slack.Client) string {
+func get_calendar_channel(api *slack.Client, channel_name string) string {
 	var result string
-	calender_name := "calendar" // Target name of channel
 
 	prm := slack.GetConversationsParameters{}
 	channels, _, err := api.GetConversations(&prm)
@@ -56,10 +59,18 @@ func get_calendar_channel(api *slack.Client) string {
 
 	// Find channel
 	for _, c := range channels {
-		if strings.Contains(c.Name, calender_name) {
+		if strings.Contains(c.Name, channel_name) {
 			result = c.ID
 		}
 	}
 
 	return result
+}
+
+func delete_message(api *slack.Client, chn_id string, timestamp string, cnt int) int {
+	_, _, err := api.DeleteMessage(chn_id, timestamp)
+	check(err)
+	time.Sleep(1 * time.Second) // To escape the api limit
+	cnt++
+	return cnt
 }
